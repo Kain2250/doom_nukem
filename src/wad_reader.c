64eeebf1f4ea85b76a4c18ebeca9c46368ddd3af
Bean Drinking <bdrinkin@il-g5.msk.21-school.ru>
2020-09-26 16:15:55 +0300

    add_draw_map

--- src/wad_reader.c
+++ src/wad_reader.c
@@ -6,13 +6,13 @@
 /*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 20:39:05 by bdrinkin          #+#    #+#             */
-/*   Updated: 2020/09/25 21:08:54 by bdrinkin         ###   ########.fr       */
+/*   Updated: 2020/09/26 15:53:20 by bdrinkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom_nukem.h"
 
-void		read_head_data(const uint8_t *data, int offset, t_wad_head *head)
+void			read_head_data(const uint8_t *data, int offset, t_wad_head *head)
 {
 	head->wad_type[0] = data[offset];
 	head->wad_type[1] = data[offset + 1];
@@ -23,7 +23,7 @@ void		read_head_data(const uint8_t *data, int offset, t_wad_head *head)
 	head->dir_offset = bytes_to_int(data, offset + 8);
 }
 
-void		read_dir_data(const uint8_t *data, int offset, t_dir *dir)
+void			read_dir_data(const uint8_t *data, int offset, t_dir *dir)
 {
 	dir->lump_offset = bytes_to_int(data, offset);
 	dir->lump_size = bytes_to_int(data, offset + 4);
@@ -38,7 +38,7 @@ void		read_dir_data(const uint8_t *data, int offset, t_dir *dir)
 	dir->lump_name[8] = '\0';
 }
 
-void		wad_put(char *name, int offset, int size)
+void			wad_put(char *name, int offset, int size)
 {
 	ft_putstr("Offset - ");
 	ft_putnbr(offset);
@@ -50,7 +50,7 @@ void		wad_put(char *name, int offset, int size)
 	ft_putchar('\n');
 }
 
-void		read_linedef(const uint8_t *data, int offset, t_linedef *linedef)
+void			read_linedef(const uint8_t *data, int offset, t_linedef *linedef)
 {
 	linedef->start = bytes_to_short(data, offset);
 	linedef->finish = bytes_to_short(data, offset + 2);
@@ -61,13 +61,13 @@ void		read_linedef(const uint8_t *data, int offset, t_linedef *linedef)
 	linedef->rear = bytes_to_short(data, offset + 12);
 }
 
-void	read_vertex(const uint8_t *data, int offset, t_vertex *vertex)
+void			read_vertex(const uint8_t *data, int offset, t_vertex *vertex)
 {
 	vertex->x = bytes_to_short(data, offset);
 	vertex->y = bytes_to_short(data, offset + 2);
 }		
 
-uint32_t	find_offset_lump(t_dir *dir, char *lable, char *name_map)
+uint32_t		find_offset_lump(t_dir *dir, char *lable, char *name_map)
 {
 	t_dir	*temp;
 
@@ -79,7 +79,7 @@ uint32_t	find_offset_lump(t_dir *dir, char *lable, char *name_map)
 	return (temp->lump_offset);
 }
 
-uint32_t	find_size_lump(t_dir *dir, char *lable, char *name_map)
+uint32_t		find_size_lump(t_dir *dir, char *lable, char *name_map)
 {
 	t_dir	*temp;
 
@@ -91,11 +91,70 @@ uint32_t	find_size_lump(t_dir *dir, char *lable, char *name_map)
 	return (temp->lump_size);
 }
 
-bool		wad_reader(t_doom_nukem *doom)
+void			wad_draw_linedefs(t_doom_nukem *doom, t_vertex *vertex)
 {
-	t_dir	*temp;
-	size_t	i;
-	t_vertex *vertex;
+	t_linedef	*line;
+	uint32_t 	offset;
+	uint32_t	size;
+	uint32_t	temp_offset;
+	int			i = 0;
+
+	offset = find_offset_lump(doom->wad.dir, "LINEDEFS", "E1M1");
+	size = find_size_lump(doom->wad.dir, "LINEDEFS", "E1M1");
+	line = (t_linedef *)ft_memalloc(size / 14 * sizeof(t_linedef));
+	temp_offset = offset + size;
+	while (offset < temp_offset)
+	{
+		read_linedef(doom->wad.map, offset, &line[i]);
+		offset += 14;
+		i++;
+	}
+	i = 0;
+	while (i != size / 14)
+	{
+		draw_line(doom->sdl.surface, fill_point(abs(vertex[line[i].start].x) / 5 + 100 , abs(vertex[line[i].start].y)  / 5 - 100),
+									fill_point(abs(vertex[line[i].finish].x)  / 5 + 100, abs(vertex[line[i].finish].y)  / 5 - 100),
+									0xffffff);
+		i++;
+	}
+	free(line);
+}
+
+void			wad_draw_vertex(t_doom_nukem *doom)
+{
+	t_vertex	*vertex;
+	uint32_t 	offset;
+	uint32_t	size;
+	uint32_t	temp_offset;
+	int			i;
+
+	i = 0;
+	offset = find_offset_lump(doom->wad.dir, "VERTEXES", "E1M1");
+	size = find_size_lump(doom->wad.dir, "VERTEXES", "E1M1");
+	vertex = (t_vertex *)ft_memalloc(size / 4 * sizeof(t_vertex));
+	temp_offset = offset + size;
+	while (offset < temp_offset)
+	{
+		read_vertex(doom->wad.map, offset, &vertex[i]);
+		offset += 4;
+		i++;
+	}
+	i = 0;
+	while (i != size / 4)
+	{
+		putpixel(doom->sdl.surface, abs(vertex[i].x) / 5 + 100,
+									abs(vertex[i].y) / 5 - 100, 0xffffff);
+		i++;
+	}
+	wad_draw_linedefs(doom, vertex);
+	// free(vertex);
+}
+
+
+bool			wad_reader(t_doom_nukem *doom)
+{
+	t_dir		*temp;
+	size_t		i;
 
 	i = -1;
 	read_head_data(doom->wad.map, 0, &doom->wad.head);
@@ -112,24 +171,7 @@ bool		wad_reader(t_doom_nukem *doom)
 			temp = temp->next;
 		}
 	}
-	temp = doom->wad.dir;
-	uint32_t 	offset = find_offset_lump(doom->wad.dir, "VERTEXES", "E1M1");
-	uint32_t	size = find_size_lump(doom->wad.dir, "VERTEXES", "E1M1");
-	vertex = (t_vertex *)ft_memalloc(size / 4 * sizeof(t_vertex));
-	i = 0;
-	uint32_t	temp_offset = offset + size;
-	while (offset <= temp_offset)
-	{
-		read_vertex(doom->wad.map, offset, &vertex[i]);
-		printf("x = %d y = %d\n", vertex[i].x, vertex[i].y);
-		offset += 4;
-		i++;
-	}
-	free(vertex);
-	// while (temp != NULL)
-	// {
-	// 	wad_put(temp->lump_name, temp->lump_offset, temp->lump_size);
-	// 	temp = temp->next;
-	// }
+	wad_draw_vertex(doom);
+
 	return (true);
 }
