600103df20ca38c94c63c77edcd10d82266e2da0
Bean Drinking <bdrinkin@am-c2.msk.21-school.ru>
2020-10-09 20:57:32 +0300

    /

--- src/wad/bytes_shift.c
+++ src/wad/bytes_shift.c
@@ -6,11 +6,16 @@
 /*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 20:38:16 by bdrinkin          #+#    #+#             */
-/*   Updated: 2020/10/02 18:12:15 by bdrinkin         ###   ########.fr       */
+/*   Updated: 2020/10/09 20:43:36 by bdrinkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom_nukem.h"
+
+int16_t		bytes_to_ishort(const uint8_t *data, int offset)
+{
+	return ((data[offset + 1] << 8) | data[offset]);
+}
 
 uint16_t	bytes_to_short(const uint8_t *data, int offset)
 {
